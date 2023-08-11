2dab1d6a72d56be49fd15d14378c0a0dbdbee1be
Federico Correa <federico.correa.lima@gmail.com>
2023-08-11 11:48:28 -0300

    feat: get user by id and use get it in middleware

--- routes/user/[id].dart
+++ routes/user/[id].dart
@@ -1,5 +1,15 @@
-import 'package:dart_frog/dart_frog.dart';
+import 'dart:io';
 
-Response onRequest(RequestContext context, String id) {
-  return Response(body: 'search user $id');
+import 'package:dart_frog/dart_frog.dart';
+import 'package:my_project/controllers/user_controller.dart';
+import 'package:my_project/models/network_error.dart';
+
+Future<Response> onRequest(RequestContext context, String id) async {
+  if (context.request.method != HttpMethod.get) {
+    throw NetworkError(
+      code: HttpStatus.methodNotAllowed,
+      message: 'Method not allowed',
+    );
+  }
+  return Response.json(body: await context.read<UserController>().getUser(id));
 }
--- routes/user/_middleware.dart
+++ routes/user/_middleware.dart
@@ -1,9 +1,10 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:get_it/get_it.dart';
 import 'package:my_project/controllers/user_controller.dart';
-import 'package:my_project/sources/in_memory_users_source.dart';
+import 'package:my_project/interfaces/users_data_source.dart';
 
-final _dataSource = InMemoryUsers();
-final UserController _userController = UserController(_dataSource);
+final UserController _userController =
+    UserController(GetIt.I.get<IUsersDataSource>());
 
 Handler middleware(Handler handler) {
   return handler.use(provider<UserController>((context) => _userController));
